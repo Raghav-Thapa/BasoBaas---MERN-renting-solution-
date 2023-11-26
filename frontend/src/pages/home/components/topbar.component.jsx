c27cbfacc637887eeb66f939d82e824203acb249
Raghav Thapa <raghavthapa7@gmail.com>
2023-11-26 15:50:25 +0545

    login form

--- frontend/src/pages/home/components/topbar.component.jsx
+++ frontend/src/pages/home/components/topbar.component.jsx
@@ -6,6 +6,12 @@ import { FaFacebook, FaTwitter } from "react-icons/fa"
 import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
 import { NavLink } from "react-router-dom";
+import { Container, Form, Row, Col,InputGroup } from "react-bootstrap"
+import { HiOutlineMail, HiOutlineLockClosed } from "react-icons/hi"
+import { FaCircleUser } from "react-icons/fa6"
+import { FaRegUser } from "react-icons/fa";
+import { FaUserPlus } from "react-icons/fa6";
+import signupimage from "../../../assets/images/user.png"
 
 
 const Topbar = () => {
@@ -35,18 +41,83 @@ const Topbar = () => {
 
                                 <li to="login">
                                     <Button className="btnstyle" label="Login" onClick={() => setVisible(true)} />
-                                    <Dialog className="loginoverlay" draggable ={false} visible={visible} onHide={() => setVisible(false)}>
-                                        <p className="m-5">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                        </p>
+                                    <Dialog className="loginoverlay" draggable={false} visible={visible} onHide={() => setVisible(false)}>
+                                        <Container>
+                                            <Row>
+                                                <Col lg={6}>
+                                                    <h3 className="login logintitle"> <FaCircleUser className="mb-2 me-2" />USER LOGIN</h3>
+                                                    <hr className="mb-4" />
+
+                                                    <Form className="form-format">
+
+                                                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                                                            <InputGroup className="mb-3">
+                                                                <InputGroup.Text id="basic-addon1"><HiOutlineMail /></InputGroup.Text>
+                                                                <Form.Control
+                                                                    type="email"
+                                                                    placeholder="Enter email"
+                                                                    name="email"
+
+                                                                />
+                                                                {/* <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text> */}
+                                                                {/* <span className="text-danger">
+                                    {formik.errors?.email}
+                                </span> */}
+                                                            </InputGroup>
+                                                            <span className="text-danger"></span>
+                                                        </Form.Group>
+
+                                                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                                                            <InputGroup className="mb-3">
+                                                                <InputGroup.Text id="basic-addon1">< HiOutlineLockClosed /></InputGroup.Text>
+                                                                <Form.Control
+                                                                    type="password"
+                                                                    placeholder="Enter Password"
+                                                                    name="password"
+                                                                    
+                                                                />
+                                                            </InputGroup>
+                                                            <span className="text-danger"></span>
+                                                        </Form.Group>
+
+
+                                                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                                            <Form.Check type="checkbox" label="Remember Me" />
+                                                        </Form.Group>
+                                                        <Button variant="primary" type="submit" className="me-4 ms-2 loginbutton mt-3">
+                                                        <FaRegUser className="me-3 " />Login
+                                                        </Button>
+                                                        <a className="signup" style={{textDecoration:"none", color:"blue"}} href="forget-password">Forgot Password ? </a>
+                                                    </Form>
+                                                </Col>
+                                               
+                                                <Col lg ={1}>
+                                                    <div className="vline">
+                                                    </div>
+                                                    <div>
+                                                        <b style={{fontSize:"16px"}}>OR</b>
+                                                    </div>
+                                                    <div className="vline"></div>
+                                                </Col>
+
+                                                <Col lg={5} className="signupimage">
+                                                    <div className="formatsignup">
+                                                    <Button variant="primary" type="submit" className="me-4 ms-2 signupbutton">
+                                                        <FaUserPlus className="me-3 " />Signup
+                                                        </Button>
+                                                    </div>
+                                                </Col>
+                                
+                                            </Row>
+
+                                        </Container>
                                     </Dialog>
                                 </li>
 
                                 <li>
-                                <NavLink to= "/register"><Button className="btnstyle" style={{marginLeft:'-33px'}} label=" Register"/> </NavLink> </li>
+                                    <NavLink to="/register"><Button className="btnstyle" style={{ marginLeft: '-33px' }} label=" Register" /> </NavLink> </li>
                                 <li><hr className="dropdown-divider" /></li>
                                 <li><a className="dropdown-item" href="#!">Logout</a></li>
                             </ul>
